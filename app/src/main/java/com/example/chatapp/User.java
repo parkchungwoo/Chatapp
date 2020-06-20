21d8707101e0952b0bb421b758ac965daaf0ff67
ParkChungwoo <skdhfl159753@naver.com>
2020-06-21 04:45:43 +0900

    users fragment resolve error

--- app/src/main/java/com/example/chatapp/User.java
+++ app/src/main/java/com/example/chatapp/User.java
@@ -2,12 +2,20 @@ package com.example.chatapp;
 
 public class User {
     String email;
-
+    String profileImageUrl;
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
     }
 }
--- app/src/main/java/com/example/chatapp/ui/home/UsersFragment.java
+++ app/src/main/java/com/example/chatapp/ui/home/UsersFragment.java
@@ -2,11 +2,14 @@ package com.example.chatapp.ui.home;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.icu.text.Transliterator;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,6 +17,8 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.chatapp.R;
 import com.example.chatapp.User;
 import com.example.chatapp.UserAdapter;
@@ -36,71 +41,71 @@ public class UsersFragment extends Fragment {
     private ArrayList<User> userArrayList;
     private UserAdapter mAdapter;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-    }
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_users, container, false);
-        database = FirebaseDatabase.getInstance();
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.users_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(inflater.getContext()));
+        recyclerView.setAdapter(new UserFragmentRecyclerViewAdapter());
         userArrayList = new ArrayList<>();
-        final RecyclerView recyclerView = view.findViewById(R.id.users_recycler_view);
+        return view;
+    }
 
-//        recyclerView = (RecyclerView) rootView.findViewById(R.id.my_recycler_view);
-//        homeViewModel = new ViewModelProvider(UsersFragment.this).get(HomeViewModel.class);
-//        friendsViewModel =
-//                ViewModelProviders.of(this).get(DashboardViewModel.class);
-//        View root = inflater.inflate(R.layout.fragment_users, container, false);
-//        final TextView textView = root.findViewById(R.id.text_home);
-//        friendsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
-        SharedPreferences sharedPref = getActivity().getSharedPreferences("shared", Context.MODE_PRIVATE);
-        String stEmail = sharedPref.getString("email","");
+    class UserFragmentRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
 
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true);
+        public UserFragmentRecyclerViewAdapter() {
+            userArrayList = new ArrayList<>();
+            FirebaseDatabase.getInstance().getReference().child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    userArrayList.clear();
+                    for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        userArrayList.add(snapshot.getValue(User.class));
+                    }
+                    notifyDataSetChanged();
+                }
 
-        // use a linear layout manager
-        layoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(layoutManager);
-
-        // specify an adapter (see also next example)
-        mAdapter = new UserAdapter(userArrayList, stEmail);
-        recyclerView.setAdapter(mAdapter);
-
-        DatabaseReference ref = database.getReference("user");
-
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                Log.d(TAG, "onDataChange: " + dataSnapshot.getValue().toString());
-                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
-
-                    User user = dataSnapshot1.getValue(User.class);
-                    Log.d(TAG, "user: " + user.getEmail());
-                    userArrayList.add(user);
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
 
                 }
-                mAdapter.notifyDataSetChanged();
+            });
+        }
 
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_item_view, parent, false);
+
+            return new CustomViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            Glide.with
+                    (holder.itemView.getContext())
+                    .load(userArrayList.get(position).getProfileImageUrl())
+                    .apply(new RequestOptions().circleCrop())
+                    .into(((CustomViewHolder)holder).imageView);
+            ((CustomViewHolder)holder).textView.setText(userArrayList.get(position).getEmail());
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return userArrayList.size();
+        }
+        private class CustomViewHolder extends RecyclerView.ViewHolder {
+            public ImageView imageView;
+            public TextView textView;
+
+            public CustomViewHolder(View view) {
+                super(view);
+                imageView = (ImageView) view.findViewById(R.id.ivUser);
+                textView = (TextView) view.findViewById(R.id.tvUser);
             }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-        return view;
+        }
     }
 }
